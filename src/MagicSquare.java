2544988803a1b87bf3e8e8dc3eee3940798a601b
brmntyprytm <bramantyopriyou@gmail.com>
2023-03-14 17:06:02 +0700

    I forgor 2 💀

--- src/MagicSquare.java
+++ src/MagicSquare.java
@@ -1,27 +1,52 @@
+// NAME : Bramantyo Priyo Utomo
+// NPM : 2206821563
+// TA : SA
+// CLASS : KKI
+// GITHUB REPO : https://github.com/brmntyprytm/Assignment2.git
+// COLLABORATORS : Daffa Farras Putra Tarigan, Muhammad Fahmi Habibie
+//
+// START OF THE PROGRAM!!!
+
 public class MagicSquare {
+    // the 2D array to hold the numbers in the square
     private final int[][] square;
+    // the size of the square (i.e. the number of rows and columns)
     private final int size;
 
+    // constructor to create the magic square
     public MagicSquare(int size) {
         this.size = size;
+        // create a new 2D array with 'size' rows and 'size' columns
         square = new int[size][size];
+
+        // start with the number 1 in the middle column of the bottom row
         int num = 1;
         int row = size - 1;
         int col = size / 2;
+
+        // loop until all cells in the square have been filled
         while (num <= size * size) {
+            // put the current number in the current cell
             square[row][col] = num;
             num++;
+
+            // move to the next cell
             int nextRow = (row + 1) % size;
             int nextCol = (col + 1) % size;
+
+            // if the next cell is already filled, move up one row instead
             if (square[nextRow][nextCol] != 0) {
                 nextRow = (row - 1 + size) % size;
                 nextCol = col;
             }
+
+            // update the row and column variables for the next iteration
             row = nextRow;
             col = nextCol;
         }
     }
 
+    // method to calculate the sum of the diagonal from top-left to bottom-right
     public int diagonalSum() {
         int sum = 0;
         for (int i = 0; i < size; i++) {
@@ -30,6 +55,7 @@ public class MagicSquare {
         return sum;
     }
 
+    // method to calculate the sum of a specified column
     public int columnSum(int col) {
         int sum = 0;
         for (int row = 0; row < size; row++) {
@@ -38,6 +64,7 @@ public class MagicSquare {
         return sum;
     }
 
+    // method to calculate the sum of a specified row
     public int rowSum(int row) {
         int sum = 0;
         for (int col = 0; col < size; col++) {
@@ -46,10 +73,12 @@ public class MagicSquare {
         return sum;
     }
 
+    // method to create a string representation of the square
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
+                // use formatting to ensure each number takes up 5 spaces
                 sb.append(String.format("%5d", square[row][col]));
             }
             sb.append("\n");
--- src/TestMagicSquare.java
+++ src/TestMagicSquare.java
@@ -1,18 +1,31 @@
+// NAME : Bramantyo Priyo Utomo
+// NPM : 2206821563
+// TA : SA
+// CLASS : KKI
+// GITHUB REPO : https://github.com/brmntyprytm/Assignment2.git
+// COLLABORATORS : Daffa Farras Putra Tarigan, Muhammad Fahmi Habibie
+//
+// START OF THE PROGRAM!!!
+
 public class TestMagicSquare {
-
+    /**
+     The main method takes a single command line argument, the size of the square, and creates
+     a MagicSquare object of that size. It then prints out the square, as well as the sum of
+     the main diagonal, a specific column, and a specific row.
+     @param args the command line arguments, where the first argument is the size of the square.
+     */
     public static void main(String[] args) {
-
+        // Check if the correct number of command line arguments have been provided
         int n = 0;
-
         if (args.length == 1) {
             n = Integer.parseInt(args[0]);
         } else {
             System.out.println("Usage: java -jar <jarFile> <size of square>");
             System.exit(1);
         }
-
+        // Create a new MagicSquare object of size n
         MagicSquare ms = new MagicSquare(n);
-
+        // Print out the Magic Square, the sum of the main diagonal, a specific column, and a specific row
         System.out.println("Magic Square of size " + n + "x" + n + ":\n\n" + ms +
                 "\nMain diagonal sum: " + ms.diagonalSum() +
                 "\nColumn sum: " + ms.columnSum(0) +
